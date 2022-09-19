f0b56264dc5bd15642a9cce1ea6f22623a4c7ff9
amargh2 <margherioa@gmail.com>
2022-09-19 13:42:11 -0500

    slight fix

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -7,7 +7,7 @@ import Footer from "./Footer";
 const ProjectPage = () => {
   return (
     <div className="bg-cirrus">
-      <div className="flex flex-col justify-center">
+      <div className="flex flex-col justify-center mx-10">
         <Nav/>
         <Blurb/>
         <ProjectDisplay/>
