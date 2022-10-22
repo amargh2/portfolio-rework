e4d6104bd91977c05fb45be66c697ebaebb58c48
amargh2 <margherioa@gmail.com>
2022-10-22 10:39:02 -0500

    changed about me description

--- src/pages/components/AboutMe.js
+++ src/pages/components/AboutMe.js
@@ -12,9 +12,11 @@ const AboutMe = () => {
             <div className="flex gap-2 justify-center items-center flex-col">
               <p className="py-2 text-center mx-20 inline-block">
                 I'm an explorer at heart with a love for tech, travel, writing, and running. After I got my BA in English, I wanted to travel and see the world,
-                so I did! As a small town kid from central Illinois, I have felt very blessed to see South Korea, Southeast Asia, and then so much more 
-                as a flight attendant and solo backpacker. As a coworker, you can count on me to stay 
-                curious and humble, collaborate with enthusiasm and genuine warmth, and work problems calmly and dilligently even under pressure.
+                so I did! As a small town kid from central Illinois, I have felt very blessed to see so much of the world as a flight attendant, English teacher, 
+                and solo backpacker. I loved my time traveling and learning so much about people, service, and kindness in the air, but now 
+                I'm aspiring to bring my skills to a different kind of cloud! 
+                As a coworker, you can count on me to stay 
+                curious and humble, collaborate with enthusiasm and genuine warmth, and work problems calmly and positively even under pressure.
               </p>
             </div>
           </div>
