dae599b940d8daa0a36f6f4ca38831cdd9ca130b
amargh2 <margherioa@gmail.com>
2022-11-05 21:59:40 -0500

    changed the ~tone~ of the about me

--- src/pages/components/AboutMe.js
+++ src/pages/components/AboutMe.js
@@ -10,13 +10,15 @@ const AboutMe = () => {
           <div className="flex-col /lg:flex-row items-center flex justify-center">
             <div className="flex w-3/4 justify-center"><img className='aspect-auto rounded-full h-60 w-50' src='./mesmall.jpg' alt='me'></img></div>
             <div className="flex gap-2 justify-center items-center flex-col">
-              <p className="py-2 text-center mx-20 inline-block">
-                I'm an explorer at heart with a love for tech, travel, writing, and running. After I got my BA in English, I wanted to travel and see the world,
-                so I did! As a small town kid from central Illinois, I have felt very blessed to see so much of the world as a flight attendant, English teacher, 
-                and solo backpacker. I loved my time traveling and learning so much about people, service, and kindness in the air, but now 
-                I'm aspiring to bring my skills to a different kind of cloud! 
-                As a coworker, you can count on me to stay 
-                curious and humble, collaborate with enthusiasm and genuine warmth, and work problems calmly and positively even under pressure.
+              <p className="py-2 text-center mx-20 inline-block"> 
+                As a small town kid from central Illinois, I grew up wanting to see the world, and 
+                I have felt very lucky to do that as a flight attendant, English teacher, 
+                and solo backpacker. That said, a lot has changed since I first started my adventures as a 20-something. Somewhere in there I went vegan,
+                met my partner, rediscovered my passion for tech, and decided to take a break to build new routines and explore new interests.
+                These days, my ideal Friday night is spent watching a movie with my partner, playing a new video game, or going for a long run 
+                with a funny podcast or fantasy audiobook.
+                Coding and tech scratch my explorer's itch. There's always something
+                new to learn, and I'm enjoying the journey.
               </p>
             </div>
           </div>
