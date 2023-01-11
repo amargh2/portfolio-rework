2e250544365be276872c1a24158a74f27319ec18
amargh2 <margherioa@gmail.com>
2023-01-11 01:13:09 -0600

    text change

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <div className='flex justify-center pt-10 pb-10'>
       <div className='flex flex-col justify-center'>
-        
+        <a href='https://www.github.com/amargh2'><FaGithub size={36} color={'white'}/></a>
       </div>
     </div>
   )
