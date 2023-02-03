4e4875630fff68c1af7ad63b989f6ca2d0d9754f
amargh2 <margherioa@gmail.com>
2023-02-02 20:07:27 -0600

    edits

--- src/pages/components/AboutMe2.js
+++ src/pages/components/AboutMe2.js
@@ -26,13 +26,13 @@ export default function AboutMe2() {
                 had caught the travel bug, and I was hooked! 
                 </p>
                 <p className="py-2 text-center mx-10 inline-block">
-                After my time in Korea, I wanted to backpack through South America after some of my friends in Korea told me about their trips there. 
+                After my time in Korea, I wanted to backpack through South America after some of my friends told me about their trips there. 
                 With a guidebook, some novels I'd been meaning to read, and an empty notebook, 
                 I hopped on a plane to Buenos Aires. 
                 I traveled through northern Argentina, Bolivia, and 
                 the southern half of Peru, saw some truly unforgettable sights,
                 and met lots of people on a similar adventure to mine. After that, I worked at Starved Rock Lodge -
-                an awesome hotel in a beautiful park staffed by some great people - and I also took some time to drive around 
+                a historic hotel in a beautiful state park staffed by some great people - and I also took some time to drive around 
                 the country in my beat up '97 Honda Accord, camping in national parks and sometimes the back seat. 
                 During that trip I got a call to interview for a flight attendant role,
                 and to my great surprise, I got the job.</p>
@@ -40,7 +40,7 @@ export default function AboutMe2() {
                 <p className='py-2 text-center mx-10 inline-block'>
                 Working as a flight attendant gave me a lot of perspective about patience, adaptability, kindness, and resilience.
                 I had the opportunity to work with new people from so many different backgrounds and perspectives every single trip, all while 
-                taking care of folks on their journeys in small, direct ways - and sometimes in big ways, depending on how the day turned out! 
+                taking care of folks on their journeys in small, direct ways - and sometimes big ways, depending on how the day shook out! 
                 While I wouldn't trade a minute of my time in the air, 
                 there were a lot of ups and downs (ba-dum-ch) in that career and lifestyle for someone who's ultimately still on the 
                 more introverted side. I knew in the back of my mind I needed to decide where I wanted to end up, but I 
@@ -54,8 +54,10 @@ export default function AboutMe2() {
                 in a home lab with virtualization software to get a lay of the land and to build some knowledge.
                 Studying these topics in many ways felt like coming home, and it still does even now. Right before 
                 heading back to flying, I started The Odin Project, and I've been
-                plugging away at it since. I love the mix of creative and technical skills coding requires, and I'm doing my best to improve on 
-                those still-nascent skills every day.  </p>
+                plugging away at learning to code ever since. I love the mix of creative and technical skills coding requires, 
+                and I'm doing my best to improve on 
+                those still-nascent skills every day. When I did return to flying, it was clear I had changed, the role had changed,
+                and it was time for me to say goodbye while I still had happy memories.  </p>
                 <p className='py-2 text-center mx-10 inline-block'>
                 I'm really lucky to say that the absolute best person I met in all 
                 my travels 
@@ -66,13 +68,14 @@ export default function AboutMe2() {
                 We have been together since, and I'm grateful for him every day. We spend a lot of time in the great 
                 indoors these days, 
                 but we also love going on road trips, hikes, and occasionally 
-                hopping on airplanes when the loads are light.</p>
+                hopping on airplanes when the loads are light. We're hoping to do more of that now that the pandemic seems to be behind us.</p>
                 <p className='py-2 text-center mx-10 inline-block'> 
                 So there you have it: I'm a former flight attendant and travel enthusiast with an English degree and some self-taught technical skills 
                 looking to work 
                 in a different kind of cloud.
                 I'm going to keep on learning, tinkering, and exploring,
-                and I hope you do too! And of course, if you'd like to work together, please don't hesitate to reach out. </p> 
+                and I hope you do that in your own life and work too, whatever that looks like for you! 
+                And of course, if you'd like to work together, please don't hesitate to reach out. </p> 
             </div>
           </div>
       </div>
