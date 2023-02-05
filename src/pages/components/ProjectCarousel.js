7dbe8b22ab9014098b52ffd7c0691d461a23d97a
anthony <margherioa@gmail.com>
2023-02-04 21:43:29 -0600

    changed carousel button gradients

--- src/pages/components/ProjectCarousel.js
+++ src/pages/components/ProjectCarousel.js
@@ -47,12 +47,12 @@ export default function ProjectCarousel() {
       </div>
           <div class="carousel h-80">{createItemsForCarousel()}</div>
         <div className="flex justify-center w-full py-2 gap-2">
-          <a href="#item1" className="btn bg-gradient-to-br from-cyan-200 to-red-200 btn-xs  text-slate-800">1</a>
-          <a href="#item2" className="btn bg-gradient-to-br from-cyan-200 to-red-200 btn-xs  text-slate-800">2</a>
-          <a href="#item3" className="btn bg-gradient-to-br from-cyan-200 to-red-200 btn-xs  text-slate-800">3</a>
-          <a href="#item4" className="btn bg-gradient-to-br from-cyan-200 to-red-200 btn-xs  text-slate-800">4</a>
-          <a href="#item5" className="btn bg-gradient-to-br from-cyan-200 to-red-200 btn-xs  text-slate-800">5</a>
-          <a href="#item6" className="btn bg-gradient-to-br from-cyan-200 to-red-200 btn-xs text-slate-800">6</a>
+          <a href="#item1" className="btn bg-gradient-to-br from-cyan-200 to-indigo-300 btn-xs  text-slate-800">1</a>
+          <a href="#item2" className="btn bg-gradient-to-br from-cyan-200 to-indigo-300 btn-xs  text-slate-800">2</a>
+          <a href="#item3" className="btn bg-gradient-to-br from-cyan-200 to-indigo-300 btn-xs  text-slate-800">3</a>
+          <a href="#item4" className="btn bg-gradient-to-br from-cyan-200 to-indigo-300 btn-xs  text-slate-800">4</a>
+          <a href="#item5" className="btn bg-gradient-to-br from-cyan-200 to-indigo-300 btn-xs  text-slate-800">5</a>
+          <a href="#item6" className="btn bg-gradient-to-br from-cyan-200 to-indigo-300 btn-xs text-slate-800">6</a>
         </div>
       </div>
   )
